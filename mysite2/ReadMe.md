3e971d3376a690fcb472c38e0ee7c8c628f6ae5b
kongfudong <43160715+kongfudong@users.noreply.github.com>
2019-01-22 22:21:57 +0800

    update login

--- mysite2/ReadMe.md
+++ mysite2/ReadMe.md
@@ -2,3 +2,4 @@
 2.2019-01-07 update user.models
 3.2019-01-17 update main
 create superuser admin@123.com/admin123
+4.完成user登陆界面，目前无法登陆，next:BVDN-6 用户信息修改 
--- mysite2/mysite2/urls.py
+++ mysite2/mysite2/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main import views as main_view
+from django.conf.urls import url,include
+from main import views as main
 
 
 urlpatterns = [
-    path(r'^$', main_view.index),
-    path('admin/', admin.site.urls),
+    url(r'^$', main.index),
+    url(r'^accounts/',include('users.urls')),
+    url(r'^admin/', admin.site.urls),
 ]

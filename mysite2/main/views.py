09ce13d350fac194d8e6dee878be6c2dffd1defe
kongfudong <43160715+kongfudong@users.noreply.github.com>
2019-01-27 19:55:19 +0800

    update

--- mysite2/main/views.py
+++ mysite2/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import json
+from users.models import User
 # Create your views here.
 
 
@@ -11,4 +12,7 @@ def accounts_profile(request):
     if request.method == 'POST':
         a = json.loads(request.body)
         print(a)
+        b=User.objects.get(email=request.user.email)
+        b.name = a['name']
+        b.save()
     return render(request, 'accounts_profile.html')

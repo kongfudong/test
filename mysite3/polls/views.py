fc6b4e03cdcbc19ca1d302f864ce0c9d070c2a89
kongfudong <43160715+kongfudong@users.noreply.github.com>
2019-02-18 22:07:47 +0800

    update

--- mysite3/polls/views.py
+++ mysite3/polls/views.py
@@ -29,6 +29,7 @@ class ResultView(generic.DetailView):
     model = Question
     template_name = 'polls/results'
 
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:

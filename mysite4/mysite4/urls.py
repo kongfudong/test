bb74b17a9fa0c645fef8f49bf5d80522fd8f723b
kongfudong <sdau_wudong@163.com>
2019-03-07 22:10:24 +0800

    构建框架，创建url和路由

--- mysite4/mysite4/urls.py
+++ mysite4/mysite4/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from login import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', views.login),
+    url(r'^index/', views.index),
+    url(r'^register/', views.register),
+    url(r'^logout/', views.logout),
 ]

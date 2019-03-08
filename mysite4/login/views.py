3a6d7b94fd768f0eff59e4752d356ab51bfb6c37
kongfudong <sdau_wudong@163.com>
2019-03-08 17:16:21 +0800

    登录页面

--- mysite4/login/views.py
+++ mysite4/login/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-
+from  . import models
 # Create your views here.
 
 
@@ -10,7 +10,21 @@ def index(request):
 
 
 def login(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        message = "所有字段必须填写！"
+        if username and password:
+            username = username.strip()
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户名不存在！"
+        return render(request,'login/login.html',{'message':message})
     return render(request, 'login/login.html')
 
 
